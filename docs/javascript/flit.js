f7e40b38549351b88e263c98a3352e7f0722ac90
phagocyte <githubphagocyte@gmail.com>
2017-01-29 03:55:47 +0000

    Add functions to rearrange on resize

---
+++ docs/javascript/flit.js
@@ -0,0 +1,171 @@
+// Arbitrary numbers chosen to ensure Long Portrait mode fits into Chrome on Android.
+// Firefox uses less vertical space for the URL bar so Long Portrait will be available in both.
+// 270 is six times 45, as board is twelve squares and logo is two.
+// 73 is the remaining 3.24444 squares that can fit into the Chrome window.
+// Scaled up until all values are integers for ease of reading.
+const THRESHOLD_LongPortraitFitToWidth = 270/(45+270+73);
+const THRESHOLD_LongPortraitFitToHeight = (45+270)/(45+270+73);
+const THRESHOLD_ShortPortraitFitToWidth = (45+270)/(270+73);
+const THRESHOLD_ShortPortraitFitToHeight = 1;
+const THRESHOLD_ShortLandscapeFitToWidth = (270+73)/(45+270);
+const THRESHOLD_ShortLandscapeFitToHeight = (45+270+73)/(45+270);
+const THRESHOLD_LongLandscapeFitToWidth = (45+270+73)/270;
+
+function reorganise() {
+  ratio = window.innerWidth / window.innerHeight;
+  if (ratio < THRESHOLD_LongPortraitFitToWidth) {arrange_LongPortraitFitToWidth();
+  } elseif (ratio < THRESHOLD_LongPortraitFitToHeight) {arrange_LongPortraitFitToHeight();
+  } elseif (ratio < THRESHOLD_ShortPortraitFitToWidth) {arrange_ShortPortraitFitToWidth();
+  } elseif (ratio < THRESHOLD_ShortPortraitFitToHeight) {arrange_ShortPortraitFitToHeight();
+  } elseif (ratio < THRESHOLD_ShortLandscapeFitToWidth) {arrange_ShortLandscapeFitToWidth();
+  } elseif (ratio < THRESHOLD_ShortLandscapeFitToHeight) {arrange_ShortLandscapeFitToHeight();
+  } elseif (ratio < THRESHOLD_LongLandscapeFitToWidth) {arrange_LongLandscapeFitToWidth();
+  } else {arrange_LongLandscapeFitToHeight();
+  }
+}
+
+function arrange_LongPortraitFitToWidth() {
+  document.getElementById('title-west').style.display = 'none';
+  document.getElementById('title-north').style.display = 'inline';
+  document.getElementById('info-east').style.display = 'none';
+  document.getElementById('info-south').style.display = 'inline';
+  
+  var w = window.innerWidth;
+  document.getElementById('title-north').style.width = '100%';
+  document.getElementById('title-north').style.height = (w/6).toString().concat('px');
+  document.getElementById('board-area').style.width = '100%';
+  document.getElementById('board-area').style.height = (w).toString().concat('px');
+  document.getElementById('info-south').style.width = '100%';
+  document.getElementById('info-south').style.height = (window.innerHeight-w*7/6).toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = (w).toString().concat('px');
+  document.getElementById('board-canvas').style.height = (w).toString().concat('px');  
+}
+
+function arrange_LongPortraitFitToHeight() {
+  document.getElementById('title-west').style.display = 'none';
+  document.getElementById('title-north').style.display = 'inline';
+  document.getElementById('info-east').style.display = 'none';
+  document.getElementById('info-south').style.display = 'inline';
+  
+  var w = window.innerHeight * THRESHOLD_LongPortraitFitToWidth;
+  document.getElementById('title-north').style.width = w.toString().concat('px');
+  document.getElementById('title-north').style.height = (w/6).toString().concat('px');
+  document.getElementById('board-area').style.width = w.toString().concat('px');
+  document.getElementById('board-area').style.height = w.toString().concat('px');
+  document.getElementById('info-south').style.width = w.toString().concat('px');
+  document.getElementById('info-south').style.height = (window.innerHeight-w*7/6).toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = (w).toString().concat('px');
+  document.getElementById('board-canvas').style.height = (w).toString().concat('px');
+}
+
+function arrange_ShortPortraitFitToWidth() {
+  document.getElementById('title-north').style.display = 'none';
+  document.getElementById('title-west').style.display = 'inline';
+  document.getElementById('info-east').style.display = 'none';
+  document.getElementById('info-south').style.display = 'inline';
+  
+  var w = window.innerWidth;
+  document.getElementById('title-west').style.width = (w/7).toString().concat('px');
+  document.getElementById('title-west').style.height = (w*6/7).toString().concat('px');
+  document.getElementById('board-area').style.width = (w*6/7).toString().concat('px');
+  document.getElementById('board-area').style.height = (w*6/7).toString().concat('px');
+  document.getElementById('info-south').style.width = w.toString().concat('px');
+  document.getElementById('info-south').style.height = (window.innerHeight-w*6/7).toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = (w*6/7).toString().concat('px');
+  document.getElementById('board-canvas').style.height = (w*6/7).toString().concat('px');
+}
+
+function arrange_ShortPortraitFitToHeight() {
+  document.getElementById('title-north').style.display = 'none';
+  document.getElementById('title-west').style.display = 'inline';
+  document.getElementById('info-east').style.display = 'none';
+  document.getElementById('info-south').style.display = 'inline';
+  
+  var w = window.innerHeight * THRESHOLD_ShortPortraitFitToWidth;
+  document.getElementById('title-west').style.width = (w/7).toString().concat('px');
+  document.getElementById('title-west').style.height = (w*6/7).toString().concat('px');
+  document.getElementById('board-area').style.width = (w*6/7).toString().concat('px');
+  document.getElementById('board-area').style.height = (w*6/7).toString().concat('px');
+  document.getElementById('info-south').style.width = w.toString().concat('px');
+  document.getElementById('info-south').style.height = (window.innerHeight-w*6/7).toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = (w*6/7).toString().concat('px');
+  document.getElementById('board-canvas').style.height = (w*6/7).toString().concat('px');
+}
+
+function arrange_ShortLandscapeFitToWidth() {
+  document.getElementById('title-west').style.display = 'none';
+  document.getElementById('title-north').style.display = 'inline';
+  document.getElementById('info-south').style.display = 'none';
+  document.getElementById('info-east').style.display = 'inline';
+  
+  var h = window.innerWidth * THRESHOLD_ShortPortraitFitToWidth;
+  document.getElementById('title-north').style.width = (h*6/7).toString().concat('px');
+  document.getElementById('title-north').style.height = (h/7).toString().concat('px');
+  document.getElementById('board-area').style.width = (h*6/7).toString().concat('px');
+  document.getElementById('board-area').style.height = (h*6/7).toString().concat('px');
+  document.getElementById('info-east').style.width = (window.innerWidth-h*6/7).toString().concat('px');
+  document.getElementById('info-east').style.height = h.toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = (h*6/7).toString().concat('px');
+  document.getElementById('board-canvas').style.height = (h*6/7).toString().concat('px');
+}
+
+function arrange_ShortLandscapeFitToHeight() {
+  document.getElementById('title-west').style.display = 'none';
+  document.getElementById('title-north').style.display = 'inline';
+  document.getElementById('info-south').style.display = 'none';
+  document.getElementById('info-east').style.display = 'inline';
+  
+  var h = window.innerHeight;
+  document.getElementById('title-north').style.width = (h*6/7).toString().concat('px');
+  document.getElementById('title-north').style.height = (h/7).toString().concat('px');
+  document.getElementById('board-area').style.width = (h*6/7).toString().concat('px');
+  document.getElementById('board-area').style.height = (h*6/7).toString().concat('px');
+  document.getElementById('info-east').style.width = (window.innerWidth-h*6/7).toString().concat('px');
+  document.getElementById('info-east').style.height = h.toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = (h*6/7).toString().concat('px');
+  document.getElementById('board-canvas').style.height = (h*6/7).toString().concat('px');oncat('px');
+}
+
+function arrange_LongLandscapeFitToWidth() {
+  document.getElementById('title-north').style.display = 'none';
+  document.getElementById('title-west').style.display = 'inline';
+  document.getElementById('info-south').style.display = 'none';
+  document.getElementById('info-east').style.display = 'inline';
+  
+  var h  = window.innerWidth * THRESHOLD_LongPortraitFitToWidth;
+  document.getElementById('title-west').style.width = (h/6).toString().concat('px');
+  document.getElementById('title-west').style.height = h.toString().concat('px');
+  document.getElementById('board-area').style.width = h.toString().concat('px');
+  document.getElementById('board-area').style.height = h.toString().concat('px');
+  document.getElementById('info-east').style.width = (window.innerWidth-h*7/6).toString().concat('px');
+  document.getElementById('info-east').style.height = h.toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = h.toString().concat('px');
+  document.getElementById('board-canvas').style.height = h.toString().concat('px');
+}
+
+function arrange_LongLandscapeFitToHeight() {
+  document.getElementById('title-north').style.display = 'none';
+  document.getElementById('title-west').style.display = 'inline';
+  document.getElementById('info-south').style.display = 'none';
+  document.getElementById('info-east').style.display = 'inline';
+  
+  var h  = window.innerHeight;
+  document.getElementById('title-west').style.width = (h/6).toString().concat('px');
+  document.getElementById('title-west').style.height = h.toString().concat('px');
+  document.getElementById('board-area').style.width = h.toString().concat('px');
+  document.getElementById('board-area').style.height = h.toString().concat('px');
+  document.getElementById('info-east').style.width = (window.innerWidth-h*7/6).toString().concat('px');
+  document.getElementById('info-east').style.height = h.toString().concat('px');
+  
+  document.getElementById('board-canvas').style.width = h.toString().concat('px');
+  document.getElementById('board-canvas').style.height = h.toString().concat('px');
+}
+
+window.onresize = reorganise;
