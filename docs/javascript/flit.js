f02c7cc0e7d775b30e5dfa13fdd46afed5a7e0be
phagocyte <githubphagocyte@gmail.com>
2017-02-08 22:41:45 +0000

    Revert game over number of pieces to 48 now bug has been found

--- docs/javascript/flit.js
+++ docs/javascript/flit.js
@@ -500,7 +500,7 @@ class Game {
     }
     if (this.no_empty_neighbours(my_pieces)) {
       this.announce_game_over_due_to_constriction();
-    } else if (your_pieces.length >= 4) {
+    } else if (your_pieces.length >= 48) {
       this.announce_game_over_due_to_numbers();
     }
   }
